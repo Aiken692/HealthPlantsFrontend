80544717a1a19ecd32d8a1f15e05e9d4d476efc0
Biko23 <bikojeremy@gmail.com>
2021-03-05 11:39:11 +0300

    Changing the url to the back end heroku url to test.

--- src/components/PRCcomps/container.js
+++ src/components/PRCcomps/container.js
@@ -60,7 +60,7 @@ function Container(props){
 
 
     const getData = async () => {
-        let url ='http://localhost:5001/api/plants';
+        let url ='https://health-plants-of-uganda.herokuapp.com/';
 
         const response = await Axios.get(url);
         // console.log('response', response);
