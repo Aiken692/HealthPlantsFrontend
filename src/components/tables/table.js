15ab0a303a549a5d3096651591f2d2da727e6960
bp mike <bpmikeomo@gmail.com>
2021-03-25 20:33:37 +0300

    add plant  form

--- src/components/tables/table.js
+++ src/components/tables/table.js
@@ -1,11 +1,11 @@
 import React, { useState} from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Modal } from 'react-bootstrap';
 import styled from 'styled-components';
-import Axios from 'axios';
+import axios from 'axios';
 import * as faIcons from 'react-icons/fa'
 import * as mdIcons from 'react-icons/md'
+import { Button, Modal } from 'react-bootstrap';
 
 const Input = styled.input`
 padding:.5rem;
@@ -22,21 +22,30 @@ background-color:#CAFFCA;
 `;
 
 function Table(){
-    const [isOpen, setIsOpen] = useState(false);
+
+    const [showAddPlant, setShowAddPlant] = useState(false);
+    const handleCloseAddPlant = () => setShowAddPlant(false);
+    const handleShowAddPlant = () => setShowAddPlant(true);
+
+    const [plant_common_name, setplant_common_name] = useState("");
+    const [plant_latin_name, setplant_latin_name] = useState("");
+    const [plant_description, setplant_description] = useState("");
+    const [file, setFile ] = useState(null);
+
+//     const [isOpen, setIsOpen] = useState(false);
  
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  }
+//   const togglePopup = () => {
+//     setIsOpen(!isOpen);
+//   }
 
     const [plants, setPlants] = useState([]);
 
     const[selectedPlant, setselectedPlant] = useState({});
 
+    let url ='https://health-plants-of-uganda.herokuapp.com/api/plants';
     const getData = async () => {
         // let url ='http://localhost:5001/api/plants';
-        let url ='https://health-plants-of-uganda.herokuapp.com/api/plants';
-
-        const response = await Axios.get(url);
+        const response = await axios.get(url);
         // console.log('response', response);
         setPlants(response.data);
     }
@@ -74,9 +83,45 @@ function Table(){
             )
         })
     }
+
+    const onFormSubmit = (e)=>{
+        e.preventDefault();
+        const formData = new FormData();
+        
+        formData.append('myImage', file);
+        formData.append('plant_common_name', plant_common_name);
+        formData.append('plant_latin_name', plant_latin_name);
+        formData.append('plant_description', plant_description);
+        console.log(file);
+        const config = {
+            headers: {
+                'content-type' : 'multipart/form-data'
+            }
+        };
+    
+        axios.post(url, formData, config)
+        .then(response => {
+                    console.log(response);
+                    return;
+            //     })
+            // .then(response => {
+            // 	return;
+            }).catch(error => {
+                return;
+            })
+    }
+    
+    const onChange= (e) => {
+        const uploadedImage = e.target.files[0];	
+        setFile(uploadedImage);
+     }
+
     return (
+        <>
         <div className="Table main-content">
             <Input type="text" placeholder="search"></Input>
+            {/* <Button className="ml-4" onclick={handleShowAddPlant}>Add</Button> */}
+            <Button variant="primary" onClick={handleShowAddPlant} className="ml-4">Add</Button>
             <table className="table tableStriped table1">
                 <thead data-toggle='modal' data-target='#details'>
                     <tr>{renderHeader()}</tr>
@@ -86,6 +131,51 @@ function Table(){
                 </tbody>
             </table>
         </div>
+
+        <Modal show={showAddPlant} onHide={handleCloseAddPlant} className="addUserModal">
+            <Modal.Header closeButton className="modalHeader">
+                <Modal.Title>Add New plant</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <form onSubmit={onFormSubmit} class="row g-3">
+
+                    <div class="col-12">
+                        <label for="commonName" class="form-label">Common  Name</label>
+                        <input type="text"onChange={(event) => {
+                            setplant_common_name(event.target.value)
+                        }} class="form-control" id="commonName" placeholder=""/>
+                    </div>
+                    <div class="col-12">
+                        <label for="LatinName" class="form-label">Latin Name</label>
+                        <input type="text" class="form-control" id="LatinName" placeholder="" onChange={(event) =>{
+                            setplant_latin_name(event.target.value)
+                        }}/>
+                    </div>
+                    <div class="col-12">
+                        <label for="plantDescription" class="form-label">Description</label>
+                        <input type="test" onChange={(event) => {
+                            setplant_description(event.target.value)
+                        }} class="form-control" id="plantDescription" placeholder="" />
+                    </div>
+                    <div class="col-12">
+                        <label for="myImage" class="form-label">Image:</label>
+                        <input type='file' name='myImage' onChange={onChange}/>
+                    </div>
+                
+                    <Modal.Footer className="modalFooter">
+                        <Button variant="secondary" className="add" type="submit">
+                            Add
+                        </Button>
+                        <Button variant="primary" onClick={handleCloseAddPlant} className="cancel">
+                            Cancel
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal.Body>
+
+    </Modal>
+    
+    </>
     )
 
 }
